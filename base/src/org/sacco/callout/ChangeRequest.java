5f44d03a02253b9420c00f44d79fdaf82c1d4e67
user <user@zenith>
2019-05-07 11:59:52 +0300

    wsw

--- base/src/org/sacco/callout/ChangeRequest.java
+++ base/src/org/sacco/callout/ChangeRequest.java
@@ -27,7 +27,7 @@ public class ChangeRequest extends CalloutEngine {
 		} // org.sacco.callout.ChangeRequest.newRecord
 		return NO_ERROR;
 	}
-
+//
 	public String selectEmployer(Properties ctx, int WindowNo, GridTab mTab, GridField mField, Object value) {
 		if (value == null)
 			return "";
